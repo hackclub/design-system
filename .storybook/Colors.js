8cdab73ce49072ca2e521f2b873780293e87cf2f
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-23 18:24:05 -0500

    Update color palette story

--- .storybook/Colors.js
+++ .storybook/Colors.js
@@ -13,35 +13,16 @@ const keys = Object.keys(theme.colors).filter(
 
 const next = keys.map(key => ({ key, value: theme.colors[key] }))
 
-const Chip = props =>
-  props.color ? (
-    <RelativeBox width={1} px={5} py={4} bg={props.color} />
-  ) : (
-    <Box width={1} px={5} py={4}>
-      N/A
-    </Box>
-  )
+const Chip = props => <Box w={1} p={5} {...props} />
 
 const Pre = Text.withComponent('pre')
 
-const Card = props => (
+const Card = ({ name, color }) => (
   <Box>
-    <Chip name={props.name} color={props.color} />
-    <Text f={0}>{props.name}</Text>
-    <Pre m={0}>{props.color}</Pre>
+    <Chip name={name} bg={color} />
+    <Text f={0}>{name}</Text>
+    <Pre m={0}>{color}</Pre>
   </Box>
-)
-
-const Comparison = ({ keys }) => (
-  <Flex wrap>
-    {keys.map(key => (
-      <Flex key={key} w={[1, 1 / 2, null, 1 / 3, 1 / 4]} p={3}>
-        <Box w={1 / 2}>
-          <Card name={key} color={theme.colors[key]} />
-        </Box>
-      </Flex>
-    ))}
-  </Flex>
 )
 
 storiesOf('Color', module).add('Palette', () => (
