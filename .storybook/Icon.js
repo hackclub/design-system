1e673995a30f827b2679a3ffe8d1cd7620080e9d
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-23 18:26:30 -0500

    Fix colors on Icon story

--- .storybook/Icon.js
+++ .storybook/Icon.js
@@ -5,7 +5,7 @@ import icons from '../icons.js'
 
 storiesOf('Icon', module)
   .add('Icons', () => (
-    <Box p={2} color="white" bg="blue">
+    <Box p={2} color="white" bg="blue.5">
       <Flex wrap>
         {Object.keys(icons).map(name => (
           <Box key={name} w={[1 / 3, 1 / 5, 1 / 6, 1 / 8]} mx={2} my={3}>
@@ -19,8 +19,8 @@ storiesOf('Icon', module)
 
   .add('Color', () => (
     <div>
-      <Icon color="red" size={48} m={2} name="favorite" />
-      <Icon color="blue" size={48} m={2} name="build" />
-      <Icon color="green" size={48} m={2} name="done" />
+      <Icon color="red.5" size={48} m={2} name="favorite" />
+      <Icon color="blue.5" size={48} m={2} name="build" />
+      <Icon color="green.5" size={48} m={2} name="done" />
     </div>
   ))
