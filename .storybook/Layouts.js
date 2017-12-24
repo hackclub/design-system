447345004277f910a33e8ef006c6c1ca06870fbf
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-24 01:00:48 -0500

    Improve Layouts story

--- .storybook/Layouts.js
+++ .storybook/Layouts.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import styled from 'styled-components'
 import { storiesOf } from '@storybook/react'
-import { Flex, Box, Text, Icon, Image, Heading } from '../src'
+import { Flex, Box, Text, Icon, Image, Heading, Card, theme } from '../src'
 
 storiesOf('Layout Examples', module)
   .add('Grid', () => (
     <Box p={4}>
-      <Flex wrap mx={-3}>
+      <Flex wrap mx={-3} color="white">
         <Box width={[1, 1 / 2]} px={3} mb={4}>
-          <Box bg="lightGray">
+          <Box bg="indigo.4" p={3}>
             <Text>Hello</Text>
           </Box>
         </Box>
         <Box width={[1, 1 / 2]} px={3} mb={4}>
-          <Box bg="lightGray">
+          <Box bg="indigo.5" p={3}>
             <Text>Hello</Text>
           </Box>
         </Box>
@@ -21,31 +21,21 @@ storiesOf('Layout Examples', module)
     </Box>
   ))
   .add('Two-column', () => (
-    <Flex>
+    <Flex color="white">
       <Box px={3} width={1 / 4}>
-        <Box
-          bg="lightGray"
-          style={{
-            minHeight: '50vh'
-          }}
-        >
-          <Text>Hello</Text>
+        <Box bg="green.6" style={{ minHeight: '50vh' }}>
+          <Text p={3}>Hello</Text>
         </Box>
       </Box>
       <Box px={3} width={3 / 4}>
-        <Box
-          bg="lightGray"
-          style={{
-            minHeight: '50vh'
-          }}
-        >
-          <Text>Hello</Text>
+        <Box bg="teal.6" style={{ minHeight: '50vh' }}>
+          <Text p={3}>Hello</Text>
         </Box>
       </Box>
     </Flex>
   ))
   .add('Navbar', () => (
-    <Flex p={2} align="center" color="white" bg="blue">
+    <Flex p={2} align="center" color="white" bg="accent">
       <Icon name="menu" mr={2} />
       <Text bold mx={2}>
         Hello
@@ -60,30 +50,34 @@ storiesOf('Layout Examples', module)
     <Flex wrap>
       {cards.map(card => (
         <Box key={card.id} p={3} width={[1 / 2, 1 / 3, 1 / 4]}>
-          <Card {...card} />
+          <Tile {...card} />
         </Box>
       ))}
     </Flex>
   ))
 
-const Border = styled(Box)`
-  border: 1px solid ${props => props.theme.colors.lightGray};
-  border-radius: 3px;
+const Border = Box.extend`
+  border: 1px solid ${props => props.theme.colors.smoke};
+  border-radius: ${props => props.theme.radius};
 `
 
-const Card = ({ image, title, text }) => (
-  <Border p={1}>
+const Tile = ({ image, title, text }) => (
+  <Card boxShadowSize="sm">
     <Image src={image} width="512" height="256" />
-    <Heading fontSize={2} mt={1}>
-      {title}
-    </Heading>
-    <Text fontSize={1}>{text}</Text>
-  </Border>
+    <Box p={2}>
+      <Heading fontSize={2} bold>
+        {title}
+      </Heading>
+      <Text fontSize={1} color="slate">
+        {text}
+      </Text>
+    </Box>
+  </Card>
 )
 
 const cards = Array.from({ length: 12 }).map((n, i) => ({
   id: i,
   title: 'Hello' + i,
   text: 'Card',
-  image: 'http://placehold.it/512x256/007aff'
+  image: `http://placehold.it/512x256/${theme.colors.accent.replace('#', '')}`
 }))
