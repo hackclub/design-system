0ea6bbc4c55e9af6f3f11ddc299c74752aa31b1a
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-24 13:40:23 -0500

    Fix Divider test

--- src/__tests__/Divider.js
+++ src/__tests__/Divider.js
@@ -8,21 +8,9 @@ describe('Divider', () => {
     expect(json).toMatchSnapshot()
   })
 
-  test('width prop sets width', () => {
-    const json = renderer.create(<Divider width={1 / 2} />).toJSON()
-    expect(json).toMatchSnapshot()
-    expect(json).toHaveStyleRule('width', '50%')
-  })
-
-  test('m prop sets margin', () => {
-    const json = renderer.create(<Divider m={2} />).toJSON()
-    expect(json).toMatchSnapshot()
-    expect(json).toHaveStyleRule('margin', theme.space[2] + 'px')
-  })
-
   test('borderColor prop sets borderColor', () => {
-    const json = renderer.create(<Divider borderColor="blue" />).toJSON()
+    const json = renderer.create(<Divider borderColor="accent" />).toJSON()
     expect(json).toMatchSnapshot()
-    expect(json).toHaveStyleRule('border-color', theme.colors.blue)
+    expect(json).toHaveStyleRule('border-color', 'accent')
   })
 })
