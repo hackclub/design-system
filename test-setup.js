d6aee76e5c2d105629ba7e1cef21143a00d00b12
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-24 14:57:31 -0500

    Upgrade React 15.6 → 16.2

--- test-setup.js
+++ test-setup.js
@@ -1,5 +1,6 @@
 import 'jest-styled-components'
-import { configure } from 'enzyme'
-import Adapter from 'enzyme-adapter-react-15'
+import Enzyme from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
 
-configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() })
+
