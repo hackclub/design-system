40242231b1e07ab803d5673b93611229039693ab
Lachlan Campbell <lachlan.campbell@icloud.com>
2017-12-25 15:51:15 -0500

    Add info to BackgroundImage story

--- .storybook/BackgroundImage.js
+++ .storybook/BackgroundImage.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withInfo } from '@storybook/addon-info'
 import { BackgroundImage, Box, Text, Flex } from '../src'
 
 const image =
   'https://images.unsplash.com/photo-1416339684178-3a239570f315?ixlib=rb-0.3.5&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max'
 
 storiesOf('BackgroundImage', module)
-  .add('Basic', () => (
-    <BackgroundImage image={image} width={256}>
-      <Box p={4}>
-        <Text fontSize={6} bold align="center" color="white">
+  .add(
+    'BackgroundImage component',
+    withInfo({
+      inline: true,
+      text: 'A <Box /> extension for adding a CSS background-image.'
+    })(() => (
+      <BackgroundImage image={image} width={256}>
+        <Text fontSize={6} bold align="center" color="white" p={4}>
           Hello
         </Text>
-      </Box>
-    </BackgroundImage>
-  ))
+      </BackgroundImage>
+    ))
+  )
   .add('Fixed Height', () => (
     <BackgroundImage height={256} image={image} width={512}>
       <Box p={4}>
