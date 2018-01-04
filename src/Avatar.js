30ae3d8d6223622fdcfb77dc079aa80e20cbe3e9
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-03 22:42:10 -0500

    Max out Avatar border-radius

--- src/Avatar.js
+++ src/Avatar.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import theme from './theme'
 
 const Avatar = Box.withComponent('img').extend`
-  border-radius: 1024px;
+  border-radius: 9999px;
   display: inline-block;
   width: ${props => props.size};
   height: ${props => props.size};
