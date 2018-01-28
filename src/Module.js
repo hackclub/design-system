0ab7eeb2f037d8bc3f6abbc41e331fe09664e753
Lachlan Campbell <lachlan.campbell@icloud.com>
2018-01-27 19:58:05 -0500

    Make Module more flexible

--- src/Module.js
+++ src/Module.js
@@ -5,24 +5,24 @@ import Heading from './Heading'
 import Text from './Text'
 import PropTypes from 'prop-types'
 
-const ModuleBase = Flex.extend.attrs({
-  w: [1, 1 / 3],
-  p: 2,
-  flexDirection: ['row', 'column'],
-  align: ['flex-start', 'center']
-})`
+const ModuleBase = Flex.extend`
   text-align: left;
-  svg { flex-shrink: 0; }
+  svg {
+    flex-shrink: 0;
+  }
   ${props => props.theme.mediaQueries.md} {
+    align-items: center;
+    flex-direction: column;
     max-width: 20rem;
+    text-align: center;
   }
 `
 
-const Module = ({ icon, heading, body, color = 'white', ...props }) => (
-  <ModuleBase color={color}>
-    <Icon size={3 * 16} mr={[2, 0]} name={icon} color={color} />
+const Module = ({ icon, heading, body, color, ...props }) => (
+  <ModuleBase color={color} {...props}>
+    <Icon size={3 * 16} mr={[2, null, 0]} mb={1} name={icon} color={color} />
     <div>
-      <Heading.h2 my={1} fontSize={3} children={heading} />
+      <Heading.h2 mb={1} fontSize={3} children={heading} />
       <Text m={0} fontSize={2} children={body} />
     </div>
   </ModuleBase>
@@ -34,6 +34,12 @@ Module.propTypes = {
   heading: PropTypes.string.isRequired,
   body: PropTypes.string,
   icon: PropTypes.string.isRequired
+}
+
+Module.defaultProps = {
+  color: 'white',
+  p: 2,
+  w: [1, 1 / 3]
 }
 
 export default Module
