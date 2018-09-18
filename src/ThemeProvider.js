681c71bb80483704d729ef7aee5fe393977f9c64
Lachlan Campbell <lachlan@hackclub.com>
2018-09-17 22:36:12 -0400

    Indent default styles

--- src/ThemeProvider.js
+++ src/ThemeProvider.js
@@ -25,44 +25,44 @@ const fontsCss = css`
 `
 
 injectGlobal`
-* {
-  box-sizing: border-box;
-  font-weight: inherit;
-  text-rendering: optimizeLegibility;
-  -webkit-font-smoothing: antialiased;
-  -webkit-appearance: none;
-  -moz-appearance: none;
-}
+  * {
+    box-sizing: border-box;
+    font-weight: inherit;
+    text-rendering: optimizeLegibility;
+    -webkit-font-smoothing: antialiased;
+    -webkit-appearance: none;
+    -moz-appearance: none;
+  }
 
-html,
-body {
-  min-height: 100%;
-  min-width: 100%;
-}
+  html,
+  body {
+    min-height: 100%;
+    min-width: 100%;
+  }
 
-body {
-  padding: 0;
-  margin: 0;
-  font-size: ${theme.fontSizes[2]}px;
-  font-family: ${theme.font};
-  line-height: 1.5;
-  position: relative;
-  height: 100%;
-  max-height: 100%;
-  width: 100%;
-  -webkit-font-smoothing: antialiased;
-  overflow-x: hidden;
-  -webkit-overflow-scrolling: touch;
-}
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: ${theme.fontSizes[2]}px;
+    font-family: ${theme.font};
+    line-height: 1.5;
+    position: relative;
+    height: 100%;
+    max-height: 100%;
+    width: 100%;
+    -webkit-font-smoothing: antialiased;
+    overflow-x: hidden;
+    -webkit-overflow-scrolling: touch;
+  }
 
-a {
-  color: currentColor;
-  text-decoration: none;
-}
+  a {
+    color: currentColor;
+    text-decoration: none;
+  }
 
-textarea {
-  resize: none;
-}
+  textarea {
+    resize: none;
+  }
 `
 
 const ThemeProvider = ({ theme, webfonts, ...props }) => {
